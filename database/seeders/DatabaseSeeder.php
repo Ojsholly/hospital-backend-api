84251b27036a3b9b58383d85798f1464fafdfed3
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2023-03-18 02:56:38 +0100

    Added the database seeder and factory for the doctor role.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             AdminSeeder::class,
             UserSeeder::class,
+            DoctorSeeder::class,
         ]);
     }
 }
