d3f1bbd19dd9e7e59f74f84e757b06afd327d41b
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2023-03-19 19:16:55 +0100

    Added the feature tests for appointment creation using Flutterwave.

--- tests/Feature/Patient/AppointmentTest.php
+++ tests/Feature/Patient/AppointmentTest.php
@@ -113,4 +113,63 @@ class AppointmentTest extends TestCase
             'status' => AppointmentStatusEnum::PENDING,
         ]);
     }
+
+    public function test_patient_can_book_appointment_with_flutterwave()
+    {
+        $user = $this->user([
+            'email_verified_at' => now(),
+        ]);
+
+        $doctor = $this->doctor();
+
+        $appointmentCount = Appointment::count();
+
+        $data = [
+            'doctor_id' => $doctor->id,
+            'datetime' => now()->addDays(2)->format('Y-m-d H:i:s'),
+            'reason' => $this->faker->sentence,
+            'symptoms' => $this->faker->sentence,
+            'address' => $this->faker->address,
+            'payment_gateway' => 'flutterwave',
+        ];
+
+        Http::fake([
+            'https://api.flutterwave.com/v3/payments' => Http::response([
+                'status' => 'success',
+                'message' => 'Authorization URL created',
+                'data' => [
+                    'link' => 'https://flutterwave.com/redirect',
+                    'tx_ref' => 'tx_ref',
+                    'flw_ref' => 'flw_ref',
+                ],
+            ], 200),
+        ]);
+
+        $this->actingAs($user)->postJson(route('patients.appointments.store', ['patient' => $user]), $data)
+            ->assertCreated()
+            ->assertJsonStructure([
+                'status', 'message', 'data' => [
+                    'payment' => [
+                        'payment_url', 'reference', 'access_code',
+                    ],
+                    'appointment' => [
+                        'id', 'reference', 'user', 'datetime', 'reason', 'symptoms', 'diagnosis', 'prescription', 'comment', 'address', 'price',
+                        'status', 'created_at', 'updated_at',
+                    ],
+                ],
+            ]);
+
+        $this->assertEquals($appointmentCount + 1, Appointment::count());
+
+        $this->assertDatabaseHas('appointments', [
+            ['reference', '!=', null],
+            'user_id' => $user->id,
+            'doctor_id' => $doctor->id,
+            'datetime' => $data['datetime'],
+            'reason' => $data['reason'],
+            'symptoms' => $data['symptoms'],
+            'address' => $data['address'],
+            'status' => AppointmentStatusEnum::PENDING,
+        ]);
+    }
 }
